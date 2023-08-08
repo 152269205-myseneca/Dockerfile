50a0051bcab7397af55218f897fb499ba3955e1e
152269205-myseneca <myang111@myseneca.ca>
2023-08-08 14:25:52 -0400

    tml update

--- raspberrypi/iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ raspberrypi/iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -175,12 +175,12 @@ def sendtransactiondata(maintopic,mainproducerid,VIPERPORT,index,preprocesstopic
  
 #	  // check for payload  'uid=subject.reference,filter:resourceType=MedicationAdministration,payload=payload.payload~\
 
-     jsoncriteria='uid=metadata.dsn,filter:allrecords~\
-subtopics=metadata.property_name~\
-values=datapoint.value~\
-identifiers=metadata.display_name~\
-datetime=datapoint.updated_at~\
-msgid=datapoint.id~\
+     jsoncriteria='uid=Country Code,filter:allrecords~\
+subtopics=Country Name~\
+values=Value~\
+identifiers=Country Code~\
+datetime=Year~\
+msgid=Country Code~\
 latlong=lat:long'     
 
 #     jsoncriteria='uid=entry.0.resource.id,filter:allrecords~\
